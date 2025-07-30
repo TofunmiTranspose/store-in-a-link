39150a657a16311f5ed4d1438e723023f09ad653
TofunmiTranspose <tofexzyno@gmail.com>
2025-07-30 18:06:37 +0100

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -6,12 +6,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: `${import.meta.env.API_KEY}`,
-  authDomain: `${import.meta.env.AUTH_DOMAIN}`,
-  projectId: `${import.meta.env.PROJECT_ID}`,
-  storageBucket: `${import.meta.env.STORAGE_BUCKET}`,
-  messagingSenderId: `${import.meta.env.MESSAGING_SENDER_ID}`,
-  appId: `${import.meta.env.APP_ID}`,
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: `
+  import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
