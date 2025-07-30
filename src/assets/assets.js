be87b8f92290813da6bec1ea457fe46552b372f2
TofunmiTranspose <tofexzyno@gmail.com>
2025-07-30 23:12:22 +0100

    Update assets.js

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,8 +3,6 @@ import sneaker2 from "./sneaker2.webp";
 import sneaker3 from "./sneaker3.jpg";
 import flat from "./flat.jpg";
 import ankara from "./download.avif";
-import ankara1 from "./download(1).avif";
-import ankara2 from "./download(2).avif";
 
 
  const assets = {
@@ -13,7 +11,5 @@ import ankara2 from "./download(2).avif";
   sneaker3,
   flat,
   ankara,
-  ankara1,
-  ankara2,
 };
 export default assets;
